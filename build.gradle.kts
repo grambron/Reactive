aabe727ce0008178b2a0a8dbe6a44b7f154f5a8d
grambron <nastyabadikova@yandex.ru>
2022-03-20 16:40:03 +0300

    fixing build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,18 +12,18 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("org.slf4j:slf4j-api:+")
-    implementation("org.slf4j:slf4j-simple:+")
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
 
-    implementation("io.reactivex.rxjava3:rxjava:+")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.3")
 
-    implementation("org.mongodb:mongodb-driver-rx:+")
+    implementation("org.mongodb:mongodb-driver-rx:1.5.0")
 
-    implementation("io.reactivex:rxnetty-http:+")
-    implementation("io.reactivex:rxnetty-common:+")
-    implementation("io.reactivex:rxnetty-tcp:+")
 
-    implementation("io.netty:netty-all:+")
+    implementation("io.reactivex:rxnetty-http:0.5.3")
+    implementation("io.reactivex:rxnetty-common:0.5.3")
+    implementation("io.reactivex:rxnetty-tcp:0.5.3")
+    implementation("io.netty:netty-all:4.1.74.Final")
 }
 
 tasks.withType<KotlinCompile> {
